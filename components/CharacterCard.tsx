d34c9c5fee7280cb8e1791889e6dcf104a0186e1
ahmetovali <ahmetovali1903@gmail.com>
2025-02-12 01:05:34 +0300

    - Detail

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Character } from '../types/character';
-import Link from 'next/link';
+import axios from 'axios';
 
 interface CharacterCardProps {
   character: Character;
 }
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
+  const [firstSeenEpisodeName, setFirstSeenEpisodeName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchEpisode = async () => {
+      if (character.episode.length > 0) {
+        try {
+          const episodeUrl = character.episode[0];
+          const response = await axios.get(episodeUrl);
+          setFirstSeenEpisodeName(response.data.name);
+        } catch (error) {
+          console.error("Error fetching episode details:", error);
+        }
+      }
+    };
+
+    fetchEpisode();
+  }, [character]);
+
   return (
     <div className="border p-4 rounded-lg shadow-md transition-transform transform hover:scale-105">
-      <Link href={`/characters/${character.id}`}>
-        <img src={character.image} alt={character.name} className="w-full h-48 object-cover rounded-t-lg mb-2" />
-        <h2 className="text-xl font-bold">{character.name}</h2>
-        <p className="text-gray-600">{character.status} - {character.gender}</p>
-      </Link>
+      <img
+        src={character.image}
+        alt={character.name}
+        className="w-full h-48 object-cover rounded-t-lg mb-2"
+      />
+      <h2 className="text-xl font-bold">{character.name}</h2>
+      <p className="text-gray-500">Species: {character.species}</p>
+      <p className="text-gray-500">Origin: {character.origin.name}</p>
+      <p className="text-gray-500">Last Known Location: {character.location.name}</p>
+      <p className="text-gray-500">First Seen In: {firstSeenEpisodeName || 'Loading...'}</p>
     </div>
   );
 };
